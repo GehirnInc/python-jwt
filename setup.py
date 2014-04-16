d45efd9d97b092d4240f13cd09b32db3131fd371
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-04-16 20:48:44 +0900

    pep8

--- setup.py
+++ setup.py
@@ -4,20 +4,19 @@ import os
 
 from setuptools import setup, find_packages
 
+here = os.path.dirname(__file__)
 requires = []
-tests_require=[
+tests_require = [
     'nose',
     'coverage'
 ]
 
-here = os.path.dirname(__file__)
+
 def _read(name):
     try:
         return open(os.path.join(here, name)).read()
     except:
         return ""
-
-
 readme = _read("README.md")
 license = _read("LICENSE.md")
 
