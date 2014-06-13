64e7dab28aef3a3950cfd98fb992015401445995
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-06-13 19:47:05 +0900

    reimpl JWK and add some utility functions

--- jwt/jwk.py
+++ jwt/jwk.py
@@ -1,49 +1,195 @@
 # -*- coding: utf-8 -*-
 
-import json
+from Crypto.PublicKey import RSA
 
-from . import utils
-
+from jwt.utils import (
+    b64_decode,
+    b64_encode,
+    base64_to_int,
+    int_to_base64,
+)
 
 InvalidKey = type('InvalidKey', (ValueError, ), {})
+UnsupportedKeyType = type('UnsupportedKeyType', (ValueError, ), {})
+
+
+__all__ = ['JWK']
 
 
 class JWK:
 
-    @classmethod
-    def decode(cls, jwk):
-        assert isinstance(jwk, bytes)
+    REGISTRY = {}
 
-        key = json.loads(jwk.decode('utf8'))
-        if 'kty' not in key:
-            raise InvalidKey()
-
-        if key['kty'] == 'oct':
-            return OctKey.from_dict(key)
-
-
-class OctKey(JWK):
-
-    def __init__(self, params):
-        self.params = params
-
-    def __getattr__(self, name):
-        if name in self.params:
-            return self.params[name]
-
-        raise AttributeError(
-            '{obj!r} object has no attribute \'{name!s}\''.format(
-                obj=self, name=name
-            )
-        )
+    def __init__(self, impl):
+        self.impl = impl
 
     @property
-    def k(self):
-        return utils.b64_decode(self.params['k'].encode('utf8'))
+    def kty(self):
+        return self.impl.kty
+
+    @property
+    def kid(self):
+        return self.impl.kid
+
+    @property
+    def keyobj(self):
+        return self.impl.keyobj
+
+    def to_dict(self):
+        return self.impl.to_dict()
 
     @classmethod
-    def from_dict(cls, key):
-        assert key['kty'] == 'oct'
-        assert 'k' in key
+    def from_dict(cls, jwk):
+        jwk = jwk.copy()
+        try:
+            impl = cls.REGISTRY[jwk['kty']]
+        except KeyError:
+            raise UnsupportedKeyType()
+        else:
+            del jwk['kty']
 
-        return cls(key)
+            return cls(impl.from_dict(jwk))
+
+    @classmethod
+    def register(cls, kty):
+        def recv(impl):
+            assert issubclass(impl, Impl)
+
+            cls.REGISTRY[kty] = impl
+            return impl
+
+        return recv
+
+
+class Impl(dict):
+
+    def __init__(self, kty, params):
+        self.kty = kty
+        self.kid = params.get('kid')
+        if 'kid' in params:
+            del params['kid']
+        self.update(params)
+
+    def update(self, D):
+        if len(set(D.keys()) & {'kty', 'kid'}) > 0:
+            raise KeyError()
+
+        return super(Impl, self).update(D)
+
+    def to_dict(self):
+        D = self.copy()
+        D['kty'] = self.kty
+        if self.kid:
+            D['kid'] = self.kid
+
+        return D
+
+    @classmethod
+    def from_dict(cls, D):
+        D = D.copy()
+        return cls(**D)
+
+    @property
+    def keyobj(self):
+        raise NotImplementedError
+
+
+@JWK.register('oct')
+class OctKey(Impl):
+
+    def __init__(self, k, **kwargs):
+        self.k = k
+
+        super(OctKey, self).__init__('oct', kwargs)
+
+    def update(self, D):
+        if 'k' in D:
+            raise KeyError()
+
+        return super(OctKey, self).update(D)
+
+    def to_dict(self):
+        D = super(OctKey, self).to_dict()
+        D['k'] = b64_encode(self.k)
+
+        return D
+
+    @classmethod
+    def from_dict(cls, D):
+        D = D.copy()
+
+        if 'k' in D:
+            D['k'] = b64_decode(D['k'])
+
+        return super(OctKey, cls).from_dict(D)
+
+    @property
+    def keyobj(self):
+        return self.k
+
+
+@JWK.register('RSA')
+class RSAKey(Impl):
+
+    def __init__(self, n, e, d=0, **kwargs):
+        self.n = n
+        self.e = e
+
+        self.d = d
+
+        super(RSAKey, self).__init__('RSA', kwargs)
+
+    def update(self, D):
+        if len(set(D.keys()) & {'n', 'e', 'd'}) > 0:
+            raise KeyError()
+
+        return super(RSAKey, self).update(D)
+
+    def to_dict(self):
+        D = super(RSAKey, self).to_dict()
+        D['n'] = int_to_base64(self.n)
+        D['e'] = int_to_base64(self.e)
+
+        if self.d:
+            D['d'] = int_to_base64(self.d)
+
+        return D
+
+    @classmethod
+    def from_dict(cls, D):
+        D = D.copy()
+        for name in {'n', 'e', 'd'}:
+            if name not in D:
+                continue
+
+            D[name] = base64_to_int(D[name])
+
+        return super(RSAKey, cls).from_dict(D)
+
+    @property
+    def keyobj(self):
+        if self.d:
+            return RSA.construct((self.n, self.e, self.d))
+
+        return RSA.construct((self.n, self.e))
+
+
+class JWKSet(list):
+
+    def append(self, value):
+        if not isinstance(value, JWK):
+            raise ValueError()
+
+        return super(JWKSet, self).append(value)
+
+    def to_dict(self):
+        return dict(keys=[jwk.to_dict() for jwk in self])
+
+    @classmethod
+    def from_dict(cls, D):
+        inst = cls()
+
+        for jwk in D['keys']:
+            inst.append(JWK.from_dict(jwk))
+
+        return inst
--- jwt/tests/test_utils.py
+++ jwt/tests/test_utils.py
@@ -12,8 +12,8 @@ class TestUtils(unittest.TestCase):
             + b'"http://example.com/is_root":true}'
         self.assertEqual(
             utils.b64_encode(ret),
-            b'eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQog'
-            b'Imh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ',
+            'eyJpc3MiOiJqb2UiLA0KICJleHAiOjEzMDA4MTkzODAsDQog'
+            'Imh0dHA6Ly9leGFtcGxlLmNvbS9pc19yb290Ijp0cnVlfQ',
         )
 
     def test_b64_decode(self):
@@ -24,3 +24,9 @@ class TestUtils(unittest.TestCase):
             b'{"iss":"joe",\r\n "exp":1300819380,\r\n '
             b'"http://example.com/is_root":true}'
         )
+
+    def test_base64_to_int(self):
+        self.assertEqual(utils.base64_to_int(b'AQAB'), 65537)
+
+    def test_int_to_base64(self):
+        self.assertEqual(utils.int_to_base64(65537), 'AQAB')
