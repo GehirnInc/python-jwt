d909aad58f76c79fc9785b32488c12bcaece5221
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2014-06-15 16:12:48 +0900

    v0.2.2

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ license = _read("LICENSE.rst")
 
 setup(
     name='jwt',
-    version='0.2.1',
+    version='0.2.2',
     test_suite='jwt',
     author='Kohei YOSHIDA',
     author_email='kohei.yoshida@gehirn.co.jp',
