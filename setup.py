9c7b8b73ec800edf1f16b45d3f894952760933a0
Kohei YOSHIDA <kohei.yoshida@gehirn.co.jp>
2019-02-12 15:43:02 +0900

    bump version to 0.6.0

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ with open(os.path.join(here, './README.rst'), 'r') as fh:
 
 setup(
     name='jwt',
-    version='0.5.4',
+    version='0.6.0',
 
     description='JSON Web Token library for Python 3.',
     long_description=long_description,
