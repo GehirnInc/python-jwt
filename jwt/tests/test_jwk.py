3498f2e1f0b493d9b29ec88eeb7e394acb72d460
Castix <vincenzo.castiglia@axant.it>
2020-12-15 12:31:15 +0100

    tests: jwt/jwk.py 100% code coverage

--- jwt/tests/test_jwk.py
+++ jwt/tests/test_jwk.py
@@ -28,7 +28,15 @@ from jwt.jwk import (
 
     jwk_from_dict,
     jwk_from_pem,
+    jwk_from_der,
+    jwk_from_bytes,
+    jwk_from_bytes_argument_conversion,
+
+    MalformedJWKError,
+    UnsupportedKeyTypeError,
 )
+
+from pytest import raises
 
 from .helper import load_testdata
 
@@ -43,6 +51,52 @@ def test_jwk_from_pem():
 def test_jwk_from_dict():
     jwk_priv = jwk_from_dict(
         json.loads(load_testdata('rsa_privkey.json', 'r')))
+
+    assert isinstance(jwk_priv, RSAJWK)
+    assert isinstance(jwk_priv.keyobj, RSAPrivateKey)
+
+
+def test_jwk_from_dict_malformed_kty():
+    json_priv = json.loads(load_testdata('rsa_privkey.json', 'r'))
+    del json_priv['kty']
+    with raises(MalformedJWKError):
+        jwk_from_dict(json_priv)
+
+
+def test_jwk_from_dict_unsupported_kty():
+    json_priv = json.loads(load_testdata('rsa_privkey.json', 'r'))
+    json_priv['kty'] = 'unknown'
+    with raises(UnsupportedKeyTypeError):
+        jwk_from_dict(json_priv)
+
+
+def test_jwk_from_bytes_argument_conversion_confusing_name():
+    with raises(Exception) as ex:
+        @jwk_from_bytes_argument_conversion
+        def confusing(_content, *, _password=None, _backend=None):  # pylint: disable=unused-variable
+            pass
+    assert ("the wrapped function must have either public"
+            " or private in it's name" in str(ex))
+
+
+def test_jwk_from_bytes_without_load_function():
+    with raises(Exception) as ex:
+        jwk_from_bytes(b'')
+    assert 'is not Optional' in str(ex)
+
+
+def test_jwk_from_unsupported_pem():
+    with raises(UnsupportedKeyTypeError):
+        jwk_from_pem(load_testdata('dsa_privkey.pem'))
+
+
+def test_jwk_from_pem_not_deserializable():
+    with raises(UnsupportedKeyTypeError):
+        jwk_from_pem(b'')
+
+
+def test_jwk_from_der():
+    jwk_priv = jwk_from_der(load_testdata('rsa_privkey.der'))
 
     assert isinstance(jwk_priv, RSAJWK)
     assert isinstance(jwk_priv.keyobj, RSAPrivateKey)
@@ -66,6 +120,12 @@ class OctetJWKTest(unittest.TestCase):
 
     def test_to_dict(self):
         self.assertEqual(self.inst.to_dict(public_only=False), self.key_json)
+
+    def test_from_dict_missing_k(self):
+        key_json = self.key_json.copy()
+        del key_json['k']
+        with raises(MalformedJWKError):
+            OctetJWK.from_dict(key_json)
 
 
 class RSAJWKTest(unittest.TestCase):
@@ -121,3 +181,26 @@ class RSAJWKTest(unittest.TestCase):
 
         self.assertEqual(
             inst.to_dict(public_only=False), self.privkey_full_json)
+
+    def test_from_dict_priv_oth_unsupported(self):
+        with raises(UnsupportedKeyTypeError):
+            _json = self.privkey_full_json.copy()
+            _json['oth'] = 'unsupported'
+            RSAJWK.from_dict(_json)
+
+    def test_from_dict_priv_malformed_e(self):
+        with raises(MalformedJWKError):
+            _json = self.privkey_full_json.copy()
+            del _json['e']
+            RSAJWK.from_dict(_json)
+
+    def test_from_dict_priv_malformed_q(self):
+        with raises(MalformedJWKError):
+            _json = self.privkey_full_json.copy()
+            del _json['q']
+            RSAJWK.from_dict(_json)
+
+    def test_verify_invalid(self):
+        from cryptography.hazmat.primitives.hashes import SHA256
+        inst = RSAJWK.from_dict(self.privkey_full_json)
+        assert not inst.verify(b'hello everyone', b'', hash_fun=SHA256)
